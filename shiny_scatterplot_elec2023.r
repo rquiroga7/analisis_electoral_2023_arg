b412c172ff76b9b1e275a041178daff18f2762aa
rquiroga7 <rquiroga@unc.edu.ar>
2023-10-29 20:20:42 -0300

    Change doc + aesthetics
    Add changes to documentation and cosmetic changes to scatterplot

--- shiny_scatterplot_elec2023.r
+++ shiny_scatterplot_elec2023.r
@@ -16,7 +16,7 @@ ui <- fluidPage(
  titlePanel("Diferencia de votos Generales vs PASO (2023)"),
  sidebarLayout(
   sidebarPanel(
-   p("Tener paciencia, la carga del gráfico puede demorar 60 segundos o más."),
+   p('Seleccionar partido y 1 o más provincias, luego cliquear en "Graficar selección". La carga del gráfico puede demorar 60 segundos o más.'),
    selectInput("party", "Seleccione un partido/coalición:", choices = unique(df4$party)),
    checkboxInput("participacion", "Filtrar por participación <50 (probables errores):", value = FALSE),
    pickerInput("provincia",
@@ -24,7 +24,7 @@ ui <- fluidPage(
                choices = unique(df4$Provincia),
                options = list(`actions-box` = TRUE,
                               `deselect-all-text` = "Limpiar selección",
-                              `select-all-text` = "Todas", 
+                              `select-all-text` = "Todas",
                               `none-selected-text` = "Seleccionar al menos 1 provincia"),
                # selected = "Todas",
                multiple = T),
@@ -37,84 +37,90 @@ ui <- fluidPage(
      a("Github.",
        href = "https://github.com/rquiroga7/analisis_electoral_2023_arg",
        target = "_blank"), " Código escrito por ",
-          a("Rodrigo Quiroga",
+     a("Rodrigo Quiroga",
        href = "https://twitter.com/rquiroga777",
-       target = "_blank"),". La línea naranja indica donde deberían caer las mesas si todo el aumento de participación resultara en un aumento de votos para esta fuerza. Si las mesas se encuentran por arriba de esta línea, sugiere transferencia de votos de otras fuerzas a ésta, y por debajo indica lo contrario. Se provee una checkbox para filtrar las mesas con participación menor a 50 en PASO o generales. Como los datos son provisorios, puede haber errores de carga de telegramas.")
+       target = "_blank"),". Se agradece enormemente el aporte de ",
+     a("Juan Gabriel Juara",
+       href = "https://github.com/jgjuara",
+       target = "_blank"),
+     ", que introdujo cambios al código que permite el correcto funcionamiento de la app en Shinyapps.io. La línea naranja indica donde deberían caer las mesas si todo el aumento de participación resultara en un aumento de votos para esta fuerza. Si las mesas se encuentran por arriba de esta línea, sugiere transferencia de votos de otras fuerzas a ésta, y por debajo indica lo contrario. Se provee una checkbox para filtrar las mesas con participación menor a 50 en PASO o generales. Como los datos son provisorios, puede haber errores de carga de telegramas.")
   ),
   mainPanel(
-   conditionalPanel(condition = "input.correr > 0", 
-                    girafeOutput("scatterplot") %>% 
-      withSpinner(color="#0dc5c1")
-  ))
+   conditionalPanel(condition = "input.correr > 0",
+                    girafeOutput("scatterplot") %>%
+                     withSpinner(color="#0dc5c1")
+   ))
  )
 )
 
 # Define server
 server <- function(input, output) {
-  
 
-  observeEvent(input$correr, {  
-    
-    df4_filtered <- reactive({
-      
-      isolate({
-           if (input$participacion) {
-            df4 %>% filter(party == input$party, Provincia %in% input$provincia,
-                          participacion_paso > 50, participacion_general > 50)
-           # } else if (input$provincia == "Todas" & input$participacion){ 
-           #   df4 %>% filter(party == input$party,
-           #                  participacion_paso > 50, participacion_general > 50)
-           # } else if (input$provincia == "Todas"){
-           #  df4 %>% filter(party == input$party)
-           } else {
-             df4 %>% filter(party == input$party, Provincia %in% input$provincia)
-             
-           }
-      })
- })
-    
+
+ observeEvent(input$correr, {
+
+  df4_filtered <- reactive({
+
+   isolate({
+    if (input$participacion) {
+     df4 %>% filter(party == input$party, Provincia %in% input$provincia,
+                    participacion_paso > 50, participacion_general > 50)
+     # } else if (input$provincia == "Todas" & input$participacion){
+     #   df4 %>% filter(party == input$party,
+     #                  participacion_paso > 50, participacion_general > 50)
+     # } else if (input$provincia == "Todas"){
+     #  df4 %>% filter(party == input$party)
+    } else {
+     df4 %>% filter(party == input$party, Provincia %in% input$provincia)
+
+    }
+   })
+  })
+
 
   output$scatterplot <- renderGirafe({
-    
-    
-    gg <- ggplot(df4_filtered(), aes(
-      x = diferencia_participacion,
-      y = diferencia,
-      color = Provincia,
-      tooltip = paste0(
-        "Provincia: ",
-        Provincia,
-        "<br>",
-        "Participación PASO: ",
-        participacion_paso,
-        "<br>",
-        "Participación generales: ",
-        participacion_general,
-        "<br>",
-        "Votos PASO: ",
-        votos_PASO,
-        "<br>",
-        "Votos generales: ",
-        votos_generales,
-        "<br>",
-        "Mesa: ",
-        cod_seccion
-      ))) +
-      geom_point_interactive(alpha = 0.2) +
-      geom_abline(intercept = 0, slope = 1, color = "orange") +
-      ggtitle(paste0(input$party, " - Diferencia de votos Generales vs PASO (2023)")) +
-      labs(x = "Diferencia participación ", y = "Diferencia votos")+
-      # facet_wrap(~party, scales = "fixed") +
-      theme_light() +
-      theme(legend.position = "bottom", axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 0))
-    
-    girafe(ggobj = gg)
 
 
-      })
-  
-    
+   gg <- ggplot(df4_filtered(), aes(
+    x = diferencia_participacion,
+    y = diferencia,
+    color = Provincia,
+    tooltip = paste0(
+     "Provincia: ",
+     Provincia,
+     "<br>",
+     "Participación PASO: ",
+     participacion_paso,
+     "<br>",
+     "Participación generales: ",
+     participacion_general,
+     "<br>",
+     "Votos PASO: ",
+     votos_PASO,
+     "<br>",
+     "Votos generales: ",
+     votos_generales,
+     "<br>",
+     "Mesa: ",
+     cod_seccion
+    ))) +
+    geom_point_interactive(alpha = 0.2) +
+    geom_abline(intercept = 0, slope = 1, color = "orange") +
+    geom_hline(yintercept = 0, color="black")+
+    geom_vline(xintercept = 0, color="black")+
+    ggtitle(paste0(input$party, " - Diferencia de votos Generales vs PASO (2023)")) +
+    labs(x = "Diferencia participación ", y = "Diferencia votos")+
+    # facet_wrap(~party, scales = "fixed") +
+    theme_light() +
+    theme(legend.position = "bottom", axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 0))
+
+   girafe(ggobj = gg)
+
+
   })
+
+
+ })
 }
 
 # Run the app
